0159a1f152466d770a42209f908ee4ca9f4d59c5
martin <martin@localhost.localdomain>
2017-11-01 10:41:05 +0800

    :)

--- evaluator-logical.scm
+++ evaluator-logical.scm
@@ -1,6 +1,6 @@
 (define input-prompt ";;; Query input:")
 (define output-prompt ";;; Query output:")
-
+					;driver-loop and instantiate
 (define (query-driver-loop)
   (prompt-for-input input-prompt)
   (let ((q (query-syntax-process (read))))
@@ -19,3 +19,21 @@
 				      (contract-question-mark v))))
 	     (qeval q (singleton-stream `()))))
 	   (query-driver-loop)))))
+(define (instantiate exp frame ubound-var-handler)
+  (define (copy exp)
+    (cond ((var? exp)
+	   (let ((binding (binding-in-frame exp frame)))
+	     (if binding
+		 (copy (binding-value binding))
+		 (unbound-var-handler exp frame))))
+	  ((pair? exp)
+	   (cons (copy (car exp)) (copy (cdr exp))))
+	  (else exp)))
+  (copy exp))
+
+					;evaluator
+(define (qeval query frame-stream)
+  (let ((qproc (get (type query) `qeval)))
+    (if qproc
+	(qproc (contents query) frame-stream)
+	(simple-query query frame-stream))))
